35f35424bb918e9fe3a859bb38a230be71229d68
KarlLouie-14 <71908927+KarlLouie-14@users.noreply.github.com>
2023-05-23 11:36:11 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+/*-[ I2C Bus Scanning ]-*/
+HAL_UART_Transmit(&hlpuart1, StartMSG, sizeof(StartMSG), 10000);
+for(i=1; i<128; i++)
+{
+    ret = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i<<1), 3, 5);
+    if (ret != HAL_OK) /* No ACK Received At That Address */
+    {
+        HAL_UART_Transmit(&hlpuart1, Space, sizeof(Space), 10000);
+    }
+    else if(ret == HAL_OK)
+    {
+        sprintf(Buffer, "0x%X", i);
+        HAL_UART_Transmit(&hlpuart1, Buffer, sizeof(Buffer), 10000);
+    }
+}
+HAL_UART_Transmit(&hlpuart1, EndMSG, sizeof(EndMSG), 10000);
+/*--[ Scanning Done ]--*/
+
+
 
