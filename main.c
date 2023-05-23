dafe61bcc374c77dd8fd95f722865620c3292ee1
KarlLouie-14 <71908927+KarlLouie-14@users.noreply.github.com>
2023-05-23 11:49:54 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 // I2C BUS SCAN
 uint8_t Buffer[25] = {0};
 uint8_t Space[] = " - ";
@@ -61,8 +65,72 @@ HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
 HAL_Delay(500);
 
 ------------------------------------------------------------
- 
-    
+// Data Structure
+union RGB_DEF {
+	uint32_t bits;
+	struct {
+		uint8_t led_number;
+		uint8_t green;
+		uint8_t red;
+		uint8_t blue;
+
+	};
+};
+
+struct PINDEF {
+	GPIO_TypeDef *port; //first 4 bytes
+	uint32_t pin; //second 4 bytes
+};
+
+struct HDD {
+	struct PINDEF enable;
+	struct PINDEF detect;
+	union RGB_DEF led;
+} Hdd[HDD_SIZE];
+
+void initialize_Hdd (){
+    Hdd[ 0].enable.port = GPIOA; Hdd[ 0].enable.pin = GPIO_PIN_15; Hdd[ 0].detect.port = GPIOA; Hdd[ 0].detect.pin = GPIO_PIN_0;  Hdd[ 0].led.led_number = 0;  Hdd[ 0].led.red = 0;   Hdd[ 0].led.green = 0;   Hdd[ 0].led.blue = 0;
+	Hdd[ 1].enable.port = GPIOB; Hdd[ 1].enable.pin = GPIO_PIN_3;  Hdd[ 1].detect.port = GPIOD; Hdd[ 1].detect.pin = GPIO_PIN_2;  Hdd[ 1].led.led_number = 1;  Hdd[ 1].led.red = 0;   Hdd[ 1].led.green = 0;   Hdd[ 1].led.blue = 0;
+    //...
+    //Send
+}
+
+//main()
+volatile int diskPresent = 0;
+
+//while()
+diskPresent = 0;
+
+// Scan through the ports and see if there's any disk present
+for(int num = 0; num<MAX_LED; num++){
+  if(HAL_GPIO_ReadPin(Hdd[num].detect.port, Hdd[num].detect.pin) == GPIO_PIN_SET){
+      diskPresent = 1;
+  }
+}
+
+// If there are no disks, run the test cycle lighting all LEDs
+if (diskPresent == 0){
+  for(int num = 0; num<MAX_LED; num++){
+      Set_LED(num, 255, 255, 120);
+      WS2812_Send();
+     // HAL_Delay(1);
+  }
+  for(int num = 0; num<MAX_LED; num++){
+      Set_LED(num, 0, 0, 0);
+      WS2812_Send();
+     // HAL_Delay(1);
+  }
+}
+// Else if there is a disk present, start the main sequence which:
+else {
+  for(int num = 0; num<MAX_LED; num++){
+      if(HAL_GPIO_ReadPin(Hdd[num].detect.port, Hdd[num].detect.pin) == GPIO_PIN_SET)
+          Set_LED(num, 0, 255, 0);	// Lights up an LED to green in the disk's location
+      else
+          Set_LED(num, 255, 0, 0);	// or Lights up an LED to red where no disks are present
+  }
+  WS2812_Send();
+}
 
 
 
